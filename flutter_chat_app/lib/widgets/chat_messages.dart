1767043483b4f7fabbe4fa2ad424eb94d9304571
Elbert <elbertnatan@hotmail.com>
2023-08-14 10:59:47 -0300

    - estilizando mensagens

--- flutter_chat_app/lib/widgets/chat_messages.dart
+++ flutter_chat_app/lib/widgets/chat_messages.dart
@@ -1,11 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/widgets/message_bubble.dart';
 
 class ChatMessages extends StatelessWidget {
   const ChatMessages({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final authenticatedUser = FirebaseAuth.instance.currentUser!;
     return StreamBuilder(
       stream: FirebaseFirestore.instance
           .collection('chat')
@@ -44,15 +47,25 @@ class ChatMessages extends StatelessWidget {
             final nextChatMessage = index + 1 < loadedMessages.length
                 ? loadedMessages[index + 1].data()
                 : null;
+
             final currentMessageUserId = chatMessage['userId'];
             final nextMessageUserId =
                 nextChatMessage != null ? nextChatMessage['userId'] : null;
             final nextUserIsSame = nextMessageUserId == currentMessageUserId;
 
-            if(nextUserIsSame){
-
+            if (nextUserIsSame) {
+              return MessageBubble.next(
+                message: chatMessage['text'],
+                isMe: authenticatedUser.uid == currentMessageUserId,
+              );
+            } else {
+              return MessageBubble.first(
+                userImage: chatMessage['userImage'],
+                username: chatMessage['username'],
+                message: chatMessage['text'],
+                isMe: authenticatedUser.uid == currentMessageUserId,
+              );
             }
-
           },
         );
       },
--- flutter_chat_app/lib/widgets/new_message.dart
+++ flutter_chat_app/lib/widgets/new_message.dart
@@ -40,7 +40,7 @@ class _NewMessageState extends State<NewMessage> {
       'createdAt': Timestamp.now(),
       'userId' : currentUser.uid,
       'username' : userData.data()!['username'],
-      'userImage' : userData.data()!['image_url'],
+      'userImage' : userData.data()!['imageUrl'],
     });
   }
 
